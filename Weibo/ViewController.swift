f8eeee9c6e15e3903dd008a6c799879a703664a6
bughh <bugphh@gmail.com>
2020-04-26 09:12:58 +0800

    Initial Commit

---
+++ Weibo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weibo
+//
+//  Created by bughh on 2020/4/26.
+//  Copyright © 2020 bughh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
